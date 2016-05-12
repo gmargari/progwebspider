af6265d0b93959ee946ab213a428f31f0635a6c6
Giorgos Margaritis <gmargari@cs.uoi.gr>
2016-05-12 18:31:41 +0300

    Enable caching of web pages

--- progwebspider/settings.py
+++ progwebspider/settings.py
@@ -79,7 +79,8 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED=True
+HTTPCACHE_ENABLED=True
+HTTPCACHE_POLICY='scrapy.extensions.httpcache.RFC2616Policy'
 #HTTPCACHE_EXPIRATION_SECS=0
 #HTTPCACHE_DIR='httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES=[]
